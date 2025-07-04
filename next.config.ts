207ae5a0a44c86805e07c1755342d0dcf28e7221
Ramadhanu <rzidinc@Ramadhanus-MacBook-Pro.local>
2025-07-05 03:38:41 +0700

    cicd: serve assetPrefix to match server route

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/wcd/eka-patas" : "",
   assetPrefix: isProd ? "/wcd/eka-patas" : "",
+  images: isProd
+    ? {
+        path: "/wcd/eka-patas/_next/image/",
+      }
+    : undefined,
 
   trailingSlash: true,
 };
